ea5fb7c206d1fb98470af82c660c66eb61f5e111
Ramy <ramyfetteha@gmail.com>
2022-04-21 15:59:11 +0200

    initial commit

---
+++ src/main/java/com/example/signalflowback/SignalFlowBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.signalflowback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignalFlowBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignalFlowBackApplication.class, args);
+	}
+
+}
